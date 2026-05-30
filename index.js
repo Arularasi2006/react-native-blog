702bdaeb12a773a42ee4f02a43433342e04205e9
Arularasi2006 <sivaarularasi27@gmail.com>
2026-05-30 16:13:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import { registerRootComponent } from 'expo';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import Bio from './Bio';
+import UpdatePage from './UpdatePage';
+import Count from './Count'
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<APP />);
+// root.render(<UpdatePage />);
+// root.render(<Count />); 
+root.render(<Bio />);
+
